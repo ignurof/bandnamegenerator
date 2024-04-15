583dcc8a8c68c26a0e2a4cb11fb7fc39e2c929e5
ignurof <ignurof@ignurof.com>
2024-04-15 22:08:41 +0200

    Add event listeners to all buttons in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,6 @@ const startView = document.getElementById("start-view");
 const generatorView = document.getElementById("generator-view");
 const resultView = document.getElementById("result-view");
 
-/* startView is technically the home page, so we want to default to it */
-let activeView = startView;
-
 const usernameLogin = document.getElementById("username-login");
 const startBtn = document.getElementById("start-btn");
 
@@ -12,6 +9,10 @@ const generateBtn = document.getElementById("generate-btn");
 
 const regenerateBtn = document.getElementById("regenerate-btn");
 const tryAgainBtn = document.getElementById("tryagain-btn");
+
+/* startView is technically the home page, so we want to default to it */
+let activeView = startView;
+
 
 function changeView(value) {
     activeView.style.display = "none";
@@ -25,3 +26,18 @@ startBtn.addEventListener("click", () => {
 
     changeView(generatorView);
 }); 
+
+generateBtn.addEventListener("click", () => {
+    // disable form submission (page refresh) 
+    event.preventDefault();
+
+    changeView(resultView);
+});
+
+regenerateBtn.addEventListener("click", () => {
+    // TODO: regenerate result and update text
+});
+
+tryAgainBtn.addEventListener("click", () => {
+    changeView(startView);
+});
