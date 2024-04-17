d7110549ffdbbcfd6a9f3b6f06eb3f8c3305ab1d
ignurof <ignurof@ignurof.com>
2024-04-17 08:53:55 +0200

    Add basic setup for band name generation in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@ const startView = document.getElementById("start-view");
 const generatorView = document.getElementById("generator-view");
 const resultView = document.getElementById("result-view");
 
+/* startView is technically the home page, so we want to default to it */
+let activeView = startView;
+
 const usernameLogin = document.getElementById("username-login");
 const startBtn = document.getElementById("start-btn");
 
@@ -10,8 +13,8 @@ const generateBtn = document.getElementById("generate-btn");
 const regenerateBtn = document.getElementById("regenerate-btn");
 const tryAgainBtn = document.getElementById("tryagain-btn");
 
-/* startView is technically the home page, so we want to default to it */
-let activeView = startView;
+const resultUsername = document.getElementById("result-username");
+const resultBandName = document.getElementById("result-bandname");
 
 
 function changeView(value) {
@@ -20,22 +23,32 @@ function changeView(value) {
     activeView.style.display = "flex";
 }
 
+function generateBandName() {
+    return "new name";
+}
+
+function setBandName(value) {
+    resultBandName.innerText = value;
+}
+
 startBtn.addEventListener("click", () => {
-    // disable form submission (page refresh) 
+    // disable form submission (avoids page refresh) 
     event.preventDefault();
 
     changeView(generatorView);
 }); 
 
 generateBtn.addEventListener("click", () => {
-    // disable form submission (page refresh) 
+    // disable form submission (avoids page refresh) 
     event.preventDefault();
 
     changeView(resultView);
+
+    setBandName(generateBandName());
 });
 
 regenerateBtn.addEventListener("click", () => {
-    // TODO: regenerate result and update text
+    setBandName(generateBandName());
 });
 
 tryAgainBtn.addEventListener("click", () => {
