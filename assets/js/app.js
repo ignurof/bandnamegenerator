324b784db4803b2ca6d6a8a8f29518afc016c4ab
ignurof <ignurof@ignurof.com>
2024-04-17 09:23:28 +0200

    Add very primitive name generation to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,33 @@
+const firstWords = [
+    "zero", 
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+];
+
+const secondWords = [
+    "abra",
+    "kadabra",
+    "yayaya",
+    "plugs",
+    "certified",
+    "sand",
+    "brand",
+    "rubberbands",
+    "candy",
+    "snakes",
+    "grass",
+    "glass",
+    "door",
+    "click"
+];
+
 const startView = document.getElementById("start-view");
 const generatorView = document.getElementById("generator-view");
 const resultView = document.getElementById("result-view");
@@ -23,8 +53,22 @@ function changeView(value) {
     activeView.style.display = "flex";
 }
 
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
+function randomInteger(min, max) {
+    /* Math.random gives us a random floating-point number between 0(inclusive) and 1(exclusive)
+     * To get a larger range just multiply the range put together and add the inclusive var
+     * Floor it down to the nearest whole number so we get an integer */
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 function generateBandName() {
-    return "new name";
+    const randomFirstInteger = randomInteger(0, firstWords.length);
+    const randomSecondInteger = randomInteger(0, secondWords.length);
+
+    const firstWord = firstWords[Math.floor(randomFirstInteger / 2 * 3.6)]; 
+    const secondWord = secondWords[Math.floor(randomSecondInteger / 3 * 1.4)];
+
+    return firstWord + " " + secondWord;
 }
 
 function setBandName(value) {
